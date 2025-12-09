711515bb9075defa905c55590681f1fd2da4e250
rchen28-y <rchen28@dtechhs.org>
2025-12-08 23:51:39 -0800

    Update codechallenge.c

--- codechallenge.c
+++ codechallenge.c
@@ -3,64 +3,68 @@
 #include <time.h>
 
 int main() {
-    
-    srand(time(NULL));
 
-    int p = rand() %8;
-    int v = rand() %8;
-    int t = rand() %8;
+    char input;
     const char *person;
     const char *verb;
     const char *thing;
-    //ints for the random number and then variables
-    
-    switch (p) {
-        case 0: person = "I";
+    printf("type 'x' to generate a random sentence");
+    scanf(" %c", &input);
+
+    if (input == 'x') {
+    srand(time(NULL));
+    int p = rand() % 8;
+    int v = rand() % 8;
+    int t = rand() % 8;
+
+        switch (p) {
+        case 0: person = "I"; 
         break;
-        case 1: person = "Panos";
+        case 1: person = "Panos"; 
         break;
-        case 2: person = "My friend";
+        case 2: person = "My friend"; 
         break;
         case 3: person = "My dog";
         break;
-        case 4: person = "My cat";
+        case 4: person = "My cat"; 
         break;
-        case 5: person = "Bob";
+        case 5: person = "Bob"; 
         break;
-        case 6: person = "Mario";
+        case 6: person = "Mario"; 
         break;
         case 7: person = "Pac-Man";
         break;
-        
-    }
+        //chooses person
+        }
 
-switch (v) {
-        case 0: verb = "ate";
+        switch (v) {
+        case 0: verb = "ate"; 
         break;
         case 1: verb = "built";
         break;
         case 2: verb = "destroyed";
         break;
-        case 3: verb = "broke";
+        case 3: verb = "broke"; 
         break;
-        case 4: verb = "trashed";
+        case 4: verb = "trashed"; 
         break;
-        case 5: verb = "improved";
+        case 5: verb = "improved"; 
         break;
-        case 6: verb = "buried";
+        case 6: verb = "buried"; 
         break;
-        case 7: verb = "absolutely obliterated";
+        case 7: verb = "absolutely obliterated"; 
         break;
-}
+        //chooses verb
+        }
 
-switch (t) {
-         case 0: thing = "a sandwich";
+        switch (t) {
+        case 0: thing = "a sandwich"; 
         break;
         case 1: thing = "the world";
         break;
-        case 2: thing = "the code";
+        case 2: thing = "the code"; 
         break;
-        case 3: thing = "a building";
+        case 3: thing = "a building"; 
         break;
         case 4: thing = "my computer";
         break;
@@ -70,14 +74,14 @@ switch (t) {
         break;
         case 7: thing = "a shoe";
         break;
-}
-    //every possibility for each part of the sentence
+        //chooses thing
+        }
 
+        printf("%s %s %s.\n", person, verb, thing);  
+    } else {
+        printf("you didnt type x so you dont get that super awesome sentence");
+        //you should have typed x
+    }
 
-    
-
-   
-    printf("%s %s %s.\n", person, verb, thing);
-// the sentence
     return 0;
 }
